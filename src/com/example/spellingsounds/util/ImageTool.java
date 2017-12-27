9da849ee0836d9bcec3351ea9a9f30f065cb14a3
miguelfreelancer56577 <miguelfreelancer56577@gmail.com>
2017-12-27 14:33:21 -0600

    class to make some works over image

---
+++ src/com/example/spellingsounds/util/ImageTool.java
@@ -0,0 +1,187 @@
+package com.example.spellingsounds.util;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.UUID;
+
+import android.app.Activity;
+import android.content.ContentUris;
+import android.content.Context;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
+import android.widget.ImageView;
+
+public class ImageTool {
+	
+	private Activity ctx;
+	
+	public ImageTool(Activity ctx){
+		this.ctx = ctx; 
+	}
+	
+	public File getFileFromImageView(ImageView img) throws IOException{
+		
+		Bitmap bitmap = ((BitmapDrawable)img.getDrawable()).getBitmap();
+		
+		ByteArrayOutputStream stream = new ByteArrayOutputStream();
+		
+		bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream);
+		
+		byte[] imageInByte = stream.toByteArray();
+		
+		ByteArrayInputStream bis = new ByteArrayInputStream(imageInByte);
+		
+		String imgName = UUID.randomUUID().toString() + ".jpg";
+		
+		File imgFile = new File(ctx.getFilesDir().getAbsolutePath() + "/" + imgName);
+		
+		FileOutputStream fos = new FileOutputStream(imgFile);
+		
+		int data;
+		
+		while((data=bis.read())!=-1)
+		{ 
+			
+			char ch = (char)data;
+			
+			fos.write(ch);
+		
+		}
+		
+		fos.flush();
+		
+		fos.close();
+		
+		return imgFile;
+	} 
+	
+	public String getPath(final Uri uri) 
+    {
+		final boolean isKitKat = Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT;
+
+	    // DocumentProvider
+	    if (isKitKat && DocumentsContract.isDocumentUri(ctx, uri)) {
+	        // ExternalStorageProvider
+	        if (isExternalStorageDocument(uri)) {
+	            final String docId = DocumentsContract.getDocumentId(uri);
+	            final String[] split = docId.split(":");
+	            final String type = split[0];
+
+	            if ("primary".equalsIgnoreCase(type)) {
+	                return Environment.getExternalStorageDirectory() + "/" + split[1];
+	            }
+
+	            // TODO handle non-primary volumes
+	        }
+	        // DownloadsProvider
+	        else if (isDownloadsDocument(uri)) {
+
+	            final String id = DocumentsContract.getDocumentId(uri);
+	            final Uri contentUri = ContentUris.withAppendedId(
+	                    Uri.parse("content://downloads/public_downloads"), Long.valueOf(id));
+
+	            return getDataColumn(ctx, contentUri, null, null);
+	        }
+	        // MediaProvidery
+	        else if (isMediaDocument(uri)) {
+	            final String docId = DocumentsContract.getDocumentId(uri);
+	            final String[] split = docId.split(":");
+	            final String type = split[0];
+
+	            Uri contentUri = null;
+	            if ("image".equals(type)) {
+	                contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+	            } else if ("video".equals(type)) {
+	                contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+	            } else if ("audio".equals(type)) {
+	                contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+	            }
+
+	            final String selection = "_id=?";
+	            final String[] selectionArgs = new String[] {
+	                    split[1]
+	            };
+
+	            return getDataColumn(ctx, contentUri, selection, selectionArgs);
+	        }
+	    }
+	    // MediaStore (and general)
+	    else if ("content".equalsIgnoreCase(uri.getScheme())) {
+	        return getDataColumn(ctx, uri, null, null);
+	    }
+	    // File
+	    else if ("file".equalsIgnoreCase(uri.getScheme())) {
+	        return uri.getPath();
+	    }
+
+	    return null;
+    }
+	
+	/**
+	 * Get the value of the data column for this Uri. This is useful for
+	 * MediaStore Uris, and other file-based ContentProviders.
+	 *
+	 * @param context The context.
+	 * @param uri The Uri to query.
+	 * @param selection (Optional) Filter used in the query.
+	 * @param selectionArgs (Optional) Selection arguments used in the query.
+	 * @return The value of the _data column, which is typically a file path.
+	 */
+	protected static String getDataColumn(Context context, Uri uri, String selection,
+	        String[] selectionArgs) {
+
+	    Cursor cursor = null;
+	    final String column = "_data";
+	    final String[] projection = {
+	            column
+	    };
+
+	    try {
+	        cursor = context.getContentResolver().query(uri, projection, selection, selectionArgs,
+	                null);
+	        if (cursor != null && cursor.moveToFirst()) {
+	            final int column_index = cursor.getColumnIndexOrThrow(column);
+	            return cursor.getString(column_index);
+	        }
+	    } finally {
+	        if (cursor != null)
+	            cursor.close();
+	    }
+	    return null;
+	}
+
+
+	/**
+	 * @param uri The Uri to check.
+	 * @return Whether the Uri authority is ExternalStorageProvider.
+	 */
+	protected static boolean isExternalStorageDocument(Uri uri) {
+	    return "com.android.externalstorage.documents".equals(uri.getAuthority());
+	}
+
+	/**
+	 * @param uri The Uri to check.
+	 * @return Whether the Uri authority is DownloadsProvider.
+	 */
+	protected static boolean isDownloadsDocument(Uri uri) {
+	    return "com.android.providers.downloads.documents".equals(uri.getAuthority());
+	}
+
+	/**
+	 * @param uri The Uri to check.
+	 * @return Whether the Uri authority is MediaProvider.
+	 */
+	protected static boolean isMediaDocument(Uri uri) {
+	    return "com.android.providers.media.documents".equals(uri.getAuthority());
+	}
+	
+}
